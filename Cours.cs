37508869cbd10f2b8b5aee3a3ad5c50244e4e71a
Kodj Laurent Egbakou <egbakoukodjolaurent@gmail.com>
2020-12-06 17:24:57 +0100

    Cours class added and Program updated

---
+++ Cours.cs
@@ -0,0 +1,34 @@
+﻿using Ical.Net.CalendarComponents;
+using System;
+
+namespace ICSFileReader
+{
+    public class Cours
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+
+        // Constructor that convert CalendarEvent to Cours
+        public Cours(CalendarEvent evt)
+        {
+            Name = evt.Summary.Trim();
+            Description = evt.Description.Trim();
+            Location = evt.Location.Trim();
+            DateStart =  evt.DtStart.AsSystemLocal;
+            DateEnd = evt.DtEnd.AsSystemLocal;
+        }
+
+        public override string ToString() =>
+            $"Cours: {Name}{Environment.NewLine}" +
+            $"Description: {Description}{Environment.NewLine}" +
+            $"Location: {Location}{Environment.NewLine}" +
+            $"Start Date: {DateStart}{Environment.NewLine}" +
+            $"End Date: {DateEnd}";
+    }
+
+
+    
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Ical.Net;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ICSFileReader
 {
@@ -6,7 +10,45 @@ namespace ICSFileReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Load the .ics file
+            var calendar = Calendar.Load(File.OpenRead("cours.ics"));
+            // Get events
+            var events = calendar.Events;
+
+            // Order events by Start Date
+            var events_sorted = events.OrderBy(e => e.DtStart.AsSystemLocal);
+            
+            foreach (var evt in events_sorted)
+            {
+                // C# Target-typed new expressions
+                Cours cours = new(evt);
+                Console.WriteLine(cours);
+                Console.WriteLine("=================================");
+            }
         }
+
+        async Task ReadRemoteIcsFileAsync()
+        {
+            var ing1OlineSchedule = System.Net.WebRequest.Create("link");
+            var response = await ing1OlineSchedule.GetResponseAsync().ConfigureAwait(false);
+            var stream = response.GetResponseStream();
+            
+            var calendar = Calendar.Load(stream);
+
+            // Get events
+            var events = calendar.Events;
+
+            // Order events by Start Date
+            var events_sorted = events.OrderBy(e => e.DtStart.AsSystemLocal);
+
+            foreach (var evt in events_sorted)
+            {
+                // C# Target-typed new expressions
+                Cours cours = new(evt);
+                Console.WriteLine(cours);
+                Console.WriteLine("=================================");
+            }
+        }
+
     }
 }
